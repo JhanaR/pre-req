0184c086a93ee53a09929f702f932f3c10fe010c
JhanaR <132604023+JhanaR@users.noreply.github.com>
2024-08-21 21:14:18 +0530

    Add files via upload

--- JavaScript/ARRAYS/array.js
+++ JavaScript/ARRAYS/array.js
@@ -1,7 +1,7 @@
 let doubleArray =["apple","ball","cat","dog",[1,2]];
 
-//console.log(doubleArray);
+console.log(doubleArray);
 
 //console.log(doubleArray.length);
 
-console.log(doubleArray[4][0]);
+// console.log(doubleArray[4][0]);
--- JavaScript/LOOPS/for.js
+++ JavaScript/LOOPS/for.js
@@ -1,32 +1,59 @@
-//initialization, condition, increment/decrement are in organized way so its more meaniful than while
+//initialization, condition, increment/decrement step should be in organized way so its more meaniful than while
 //i want to print 1 to 10 using
 
+//FOR LOOP
 // for(let j=0; j<=3; j++){
 //   console.log(j);
 // }
 
-//for in loop used for object.
+
 let obj = {
   name: "heva",
   age: 3,
   hobbies: "playing"
 }
 
+
+// // //FOR IN
 for(let prop in obj)
 {
   //console.log(obj.name);
-  console.log(prop, obj[prop]);
+  // console.log(prop + ":" ,  prop);//value of properties
+  console.log(prop, obj[prop]);//value of properties
+
+}
+
+//FOR OFF
+for(let prop of Object.keys(obj))
+{
+  //console.log(obj.name);
+  // console.log('prop', prop);//properties
+  console.log(prop, obj[prop]);//value of properties
 }
 
 
-//for of loop used for arrays.
-let arr =["1", "2", "3"];
 
-// for(let k=0; k<arr.length; k++)// first try
-// {
-//   console.log(k);
-// }
 
-for(let prop of arr){///its simple
- console.log(arr);
-}
+//  let arr =["apple", "ball", "cat"];
+
+//FOR
+
+
+
+//FOR OFF
+// for(let prop of arr){///its simple
+//   //  console.log('prop', prop);//value in properties
+//   console.log(arr + ":", prop[arr]);//value in properties
+//   }
+
+// //FOR IN
+
+// for(let prop in arr){///its simple
+//     // console.log('prop', prop);//index in properties
+//    console.log(arr + ":", prop[arr]);//value in properties
+//   }
+
+
+
+
+
