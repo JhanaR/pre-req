fca57c523da23ade6cb414f42d7d03eac416a3aa
jhanaR <jhanarajendran20@gmail.com>
2025-04-30 20:06:07 +0530

    commit

---
+++ JavaScript/dataTypes.js
@@ -0,0 +1,45 @@
+// Number type
+		   let n = 123 ;
+		   let m = 123.45;
+		    //typeof n = number , typeof m = number
+			//typeof typeof n = string 
+			
+// BigInt
+		  // 2^53-1 and -(2^53-1) this is a range bound 
+		   //limitations of number 
+		   let p = 78654341234724313465564545n;
+		
+//string 
+		  let a = "hello";
+		  let b = 'hello';
+		  let c = `hello`; //template string literal 
+		  
+//Boolean 
+		true 
+        false
+		if(10>1){ }
+		
+//undefined 
+		  // In javascript you declare a variable and not assign any value then it will hold undefined it.
+		   let o;
+		   console.log(0);
+		   
+//null 
+		   let r = null;
+		   //in javascript null means "reference to non existing object "
+		   //typeof a => object 
+		   
+		   let q = 10;
+		    q = "string " 
+			// let q ;
+			 q = null;  q = {} ;
+			 
+			 q=null;  // clearing
+			 
+			 q= 20; //assigning
+			 
+			 q = "string";
+			 
+			 q = ""; // re-assigning 
+			 
+			 q =null; // clearing 
