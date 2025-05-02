71253b1478852d11183ca8b6d63f330cdfa5f510
jhanaR <jhanarajendran20@gmail.com>
2025-05-02 15:01:31 +0530

    seq

---
+++ JavaScript/loops/loop.js
@@ -0,0 +1,31 @@
+// for(var i=1;i<=10;i++){
+
+//     if(i==5)
+//       break;
+//    else
+//      console.log(i);
+ 
+//  }
+//  console.log("after loop");
+//  for(var j=1;j<=10;j++){
+
+//     if(j==5)
+//       continue;
+//    else
+//      console.log(j);
+ 
+//  }
+
+let users = [
+    { name: 'Anto', gender: 'm' },
+    { name: 'Franklin', gender: 'm' },
+    { name: 'Sam', gender: 'm' },
+    { name: 'Jasmine', gender: 'f' },
+    { name: 'Jia', gender: 'f' },
+  ]
+  
+  for (let index = 0; index < users.length; index++) {
+    const element = users[index];
+    console.log(element)
+  }
+  
